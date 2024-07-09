e00c9879ac458084792c13fb7fcd0c2ea1f0f1da
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-07-09 12:19:31 +0500

    Tailwind Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      minWidth: {
+        '8rem': '8rem',
+      },
+      colors:{
+        'primary': '#3238f2',
+      },
+      fontFamily: {
+        'acme': ['Acme' , 'sans-serif'],
+        'playfair-display': ['Playfair Display', 'serif'],
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif']
+      }
+
+    },
+  },
+  plugins: [],
+}
+
